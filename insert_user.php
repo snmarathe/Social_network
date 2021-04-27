65821e29f31e5bbf1217097692be83a406e2ad12
snmarathe <67182099+snmarathe@users.noreply.github.com>
2021-04-27 16:22:17 +0530

    Update insert_user.php

--- insert_user.php
+++ insert_user.php
@@ -2,7 +2,6 @@
 include("includes/connection.php");
 
 	if(isset($_POST['sign_up'])){
-
 		$first_name = htmlentities(mysqli_real_escape_string($con,$_POST['first_name']));
 		$last_name = htmlentities(mysqli_real_escape_string($con,$_POST['last_name']));
 		$pass = htmlentities(mysqli_real_escape_string($con,$_POST['u_pass']));
@@ -11,14 +10,20 @@ include("includes/connection.php");
 		$gender = htmlentities(mysqli_real_escape_string($con,$_POST['u_gender']));
 		$status = "verified";
 		$posts = "no";
+		$recovery = $last_name;
 
 		if(strlen($pass) <8 ){
 			echo"<script>alert('Password should have at least 8 characters!')</script>";
 			exit();
 		}
 
+		$q = "SELECT COUNT(*) FROM `users` WHERE 1";
+		$numrows = mysqli_query($con, $q);
+		$numrows_t = $numrows->fetch_array()[0] ?? '';
+
+
 		$check_email = "select * from users where email_id='$email'";
-		$run_email = mysqli_query($check_email);
+		$run_email = mysqli_query($con, $check_email);
 
 		$check = mysqli_num_rows($run_email);
 
@@ -28,21 +33,18 @@ include("includes/connection.php");
 			exit();
 		}
 
-		$rand = rand(1, 3); //Random number between 1 and 3
-
-			if($rand == 1)
-				$profile_pic = "head_red.png";
-			else if($rand == 2)
-				$profile_pic = "head_sun_flower.png";
-			else if($rand == 3)
-				$profile_pic = "head_turqoise.png";
+		if($gender == "Female"){
+			$profile_pic="f.jpg";
+		}
+		if($gender == "Male"){
+			$profile_pic="m.jpg";
+		}
 
 		$cover_pic = "default_cover.jpg";
 
-		$insert = "insert into users (first_name,last_name,email_id,country,gender,profile_pic,cover_pic,reg_date,recovery,posts,status,password)
-		values('$first_name','$last_name','$email','$country','$gender','$profile_pic','$cover_pic',NOW(),'Harry Potter','$posts','$status','$pass')";
-		
-		$query = mysqli_query($con, $insert);
+		$insert_query = "INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email_id`, `country`, `gender`, `profile_pic`, `cover_pic`, `reg_date`, `recovery`, `posts`, `status`, `password`) VALUES ('$numrows_t', '$first_name', '$last_name', '$email', '$country', '$gender', '$profile_pic', '$cover_pic', CURRENT_TIMESTAMP, '$recovery', '$posts', '$status', '$pass')";
+
+		$query = mysqli_query($con, $insert_query);
 
 		if($query){
 			echo "<script>alert('Hello $first_name, you are good to go.')</script>";
